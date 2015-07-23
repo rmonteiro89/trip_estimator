6f3f8eb97928c70ef9182848218558b1d72f45f4
Raphael Monteiro <rmonteiro89@hotmail.com>
2015-07-22 23:05:41 -0300

    First attempt to use google api auto complete

--- Gemfile
+++ Gemfile
@@ -19,6 +19,9 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 
 gem 'bootstrap-sass', '~> 3.3.5'
 
+gem 'jquery-ui-rails'
+gem 'gmaps-autocomplete-rails'
+
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
--- app/assets/javascripts/searches.js
+++ app/assets/javascripts/searches.js
@@ -1,2 +1,14 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+jQuery(function() {
+  var completer;
+
+  completer = new GmapsCompleter({
+    inputField: '#gmaps-input-address',
+    errorField: '#gmaps-error'
+  });
+
+  completer.autoCompleteInit({
+    country: "us"
+  });
+});
